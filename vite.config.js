b839a09a24488a978d431251f9310d4f3d40ca19
Eng. ebisa <ebisatesfaye30@gmail.com>
2025-11-02 16:53:31 +0300

    vite config changed

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   // ... other configuration
+  base : "/vite-test/",
   plugins: [
       react(),
       tailwindcss(),
